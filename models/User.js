4df9746e28df7367dd42f861d108e01260788762
JaredMabus <65479004+JaredMabus@users.noreply.github.com>
2022-11-08 17:15:09 -0600

    updated model and seed files

--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
-const mongoose = require("mongoose")
-const { Thought } = require('./index');
+const { Schema, Types, model } = require("mongoose")
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
   username: {
     type: String,
     required: true,
@@ -12,15 +11,24 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    match: ['/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/', "not valid email"]
+    match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, "not valid email"]
   },
-  thoughts: [ObjectId()]
+  thoughts: [{ type: Types.ObjectId, ref: 'Thought' }],
+  friends: [{ type: Types.ObjectId, ref: 'User' }],
+}, {
+  toJSON: {
+    virtuals: true,
+  },
 })
 
 
 // TO-DO: Create a virtual "friendCount" to get length of user's friends array
+userSchema
+  .virtual('friendCount')
+  .get(function () {
+    return this.friends.length;
+  });
 
-
-const User = mongoose.model("User", userSchema)
+const User = model("User", userSchema)
 
 module.exports = User
